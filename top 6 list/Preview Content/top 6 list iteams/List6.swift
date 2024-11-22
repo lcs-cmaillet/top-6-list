82c42f0f08d0506bf922ffda2ea49c9bd7052526
Collin Maillet <cmaillet@lcs.on.ca>
2024-11-22 09:52:28 -0500

    Put in and created list. Now I need to connect to content list view.

---
+++ top 6 list/Preview Content/top 6 list iteams/List6.swift
@@ -0,0 +1,18 @@
+//
+//  List6.swift
+//  top 6 list
+//
+//  Created by Collin Maillet on 2024-11-22.
+//
+
+import SwiftUI
+
+struct List6: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    List6()
+}
---
+++ top 6 list/Views/Top6ListView.swift
@@ -0,0 +1,18 @@
+//
+//  Top6ListView.swift
+//  top 6 list
+//
+//  Created by Collin Maillet on 2024-11-22.
+//
+
+import SwiftUI
+
+struct Top6ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Top6ListView()
+}
