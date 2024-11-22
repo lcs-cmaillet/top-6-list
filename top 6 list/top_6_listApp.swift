0a0f245ecb44255c3efd0c38a00cbbe47f1a0eeb
Collin Maillet <cmaillet@lcs.on.ca>
2024-11-22 08:50:56 -0500

    Initial Commit

---
+++ top 6 list/top_6_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  top_6_listApp.swift
+//  top 6 list
+//
+//  Created by Collin Maillet on 2024-11-22.
+//
+
+import SwiftUI
+
+@main
+struct top_6_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
